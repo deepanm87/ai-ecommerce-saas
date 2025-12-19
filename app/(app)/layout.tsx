3b7eeadce332d861fad3197f284f39155a9d4345
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-12-19 16:06:29 -0500

    setup header component, helper functions and rendered it

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,14 +1,19 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { SanityLive } from "@/sanity/lib/live"
 import { CartStoreProvider } from "@/lib/store/cart-store-provider"
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider"
+import { Header } from "@/components/app/Header"
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
-        <main>
-          {children}
-        </main>
+        <ChatStoreProvider>
+          <Header />
+          <main>
+            {children}
+          </main>
+        </ChatStoreProvider>
         <SanityLive />
       </CartStoreProvider>
     </ClerkProvider>
